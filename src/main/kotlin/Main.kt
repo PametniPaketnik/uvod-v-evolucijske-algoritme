33d51dce9b1adbf6f3d64956fc8edaa0e0c84664
sinisaV <sinisa.vucetic@student.um.si>
2024-01-09 13:27:28 +0100

    enabled file reading

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,25 @@
+import java.io.File
+
 fun main() {
-    println("Hello World!")
+    val file = File("src/main/resources/eil101.tsp")
+    if (file.exists()) {
+        println("File found at ${file.absolutePath}")
+    } else {
+        println("File not found!")
+    }
+
+    val path = "eil101.tsp"
+
+    //TODO set starting city, which is always at index 0
+    val inputStream = TSP::class.java.classLoader.getResourceAsStream(path)
+    if (inputStream == null) {
+        System.err.println("File $path not found!")
+        return
+    }
+    else {
+        println("File found at ${file.absolutePath}")
+        // read first line
+        val line = inputStream.bufferedReader().readLine()
+        println(line)
+    }
 }
--- src/main/kotlin/TSPTest.java
+++ src/main/kotlin/TSPTest.java
@@ -6,7 +6,7 @@ public class TSPTest {
 
         // primer zagona za problem eil101.tsp
         for (int i = 0; i < 100; i++) {
-            TSP eilTsp = new TSP("TSP files/eil101.tsp", 10000);
+            TSP eilTsp = new TSP("eil101.tsp", 10000);
             GA ga = new GA(100, 0.8, 0.1);
             TSP.Tour bestPath = ga.execute(eilTsp);
 
