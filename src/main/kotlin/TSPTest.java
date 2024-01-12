86884cc30109fbffb1e86b9f28c1cd339244ef7b
viktordenac <viktor.denac@gmail.com>
2024-01-12 01:15:46 +0100

    changed TSPTest.java for testing purposes

--- src/main/kotlin/TSPTest.java
+++ src/main/kotlin/TSPTest.java
@@ -4,15 +4,28 @@ public class TSPTest {
 
         RandomUtils.setSeedFromTime(); // nastavi novo seme ob vsakem zagonu main metode (vsak zagon bo drugačen)
 
+        double min = Integer.MAX_VALUE;
+        double avg = 0;
+        double std = 0;
+
         // primer zagona za problem eil101.tsp
-        for (int i = 0; i < 100; i++) {
-            TSP eilTsp = new TSP("bays29.tsp", 10000);
+        for (int i = 0; i < 30; i++) {
+            TSP eilTsp = new TSP("a280.tsp", 1000);
             GA ga = new GA(100, 0.8, 0.1);
             TSP.Tour bestPath = ga.execute(eilTsp);
 
             // shrani min, avg in std
+            if (bestPath.getDistance() < min) {
+                min = bestPath.getDistance();
+            }
+            avg += bestPath.getDistance();
+            std += Math.pow(bestPath.getDistance(), 2);
+
         }
         System.out.println(RandomUtils.getSeed()); // izpiše seme s katerim lahko ponovimo zagon
+        System.out.println("Min: " + min);
+        System.out.println("Avg: " + avg / 30);
+        System.out.println("Std: " + Math.sqrt(std / 30 - Math.pow(avg / 30, 2)));
 
     }
 }
